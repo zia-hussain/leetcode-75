0778de63ddaf87e7599a0f1637048e86e2ec9655
zia-hussain <syedziashahgillani76@gmail.com>
2024-09-11 14:22:30 +0500

    1768. Merge Strings Alternately (Solved)

---
+++ script.js
@@ -0,0 +1,17 @@
+// ============================================   Leetcode 75 ===============================================
+
+// ==============================   1768. Merge Strings Alternately ==============================
+
+// var mergeAlternately = function (word1, word2) {
+//   let result = "";
+//   let i = 0;
+//   while (i < word1.length || i < word2.length) {
+//     result += word1[i] || "";
+//     result += word2[i] || "";
+//     i++;
+//   }
+//   return result;
+// };
+
+// let result = mergeAlternately("zia", "shah");
+// console.log(result);
