d8b3f866d8d768febe413c51af603972c019a8fa
zia-hussain <syedziashahgillani76@gmail.com>
2024-09-16 16:35:56 +0500

    238. Product of Array Except Self (Solved)

--- script.js
+++ script.js
@@ -105,3 +105,25 @@
 
 // console.log(reverseVowels("IceCreAm"));
 // console.log(reverseVowels("leetcode"));
+
+// ==============================   238. Product of Array Except Self ==============================
+
+// function productExceptSelf(nums) {
+//   const n = nums.length;
+//   const result = new Array(n).fill(1);
+//   console.log("Result", result);
+//   let leftProduct = 1;
+//   for (let i = 0; i < n; i++) {
+//     result[i] = leftProduct;
+//     leftProduct *= nums[i];
+//   }
+
+//   let rightProduct = 1;
+//   for (let i = n - 1; i >= 0; i--) {
+//     result[i] *= rightProduct;
+//     rightProduct *= nums[i];
+//   }
+
+//   return result;
+// }
+// productExceptSelf([1, 2, 3, 4]);
